5147613ad8fc1be4209aa819a2bc9f61933bae64
sgervais333 <simon.gervais333@gmail.com>
2023-03-31 14:47:14 -0400

    Ajout de delete de file

--- Scripts/UI/LoadCampaign.cs
+++ Scripts/UI/LoadCampaign.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using Godot;
 using Godot.Collections;
@@ -12,12 +13,17 @@ using File = Godot.File;
 public class LoadCampaign : Control
 {
     private ItemList _itemList;
+    private Button _loadButton;
+    private Button _deleteButton;
     private string _selectedItem;
+    private int _selectedIndex;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _itemList = GetNode<ItemList>("MarginContainer/VBoxContainer/Control/ItemList");
+        _loadButton = GetNode<Button>("MarginContainer/VBoxContainer/HBoxContainer/LoadButton");
+        _deleteButton = GetNode<Button>("MarginContainer/VBoxContainer/DeleteContainer/DeleteButton");
         foreach (string file in Utils.FilesInDirectory(CampaignData.CampaignFolder))
         {
             _itemList.AddItem(file);
@@ -41,15 +47,32 @@ public class LoadCampaign : Control
         file.Close();
     }
 
-    public void _on_Button_pressed()
+    public void DeleteCampaignFile(string fileName)
+    {
+        Directory dir = new Directory();
+        dir.Remove($"{CampaignData.CampaignFolder}/{fileName}");
+    }
+
+    public void _on_LoadButton_pressed()
     {
         if (_selectedItem == string.Empty) return;
         LoadCampaignFile(_selectedItem);
         this.ChangeScene("res://Screens/MainCampaign.tscn", removeLastBreadcrumbScenesPath: true);
     }
 
+    public void _on_DeleteButton_pressed()
+    {
+        DeleteCampaignFile(_selectedItem);
+        _itemList.RemoveItem(_selectedIndex);
+        _loadButton.Disabled = true;
+        _deleteButton.Disabled = true;
+    }
+
     public void _on_ItemList_item_selected(int index)
     {
+        _selectedIndex = index;
         _selectedItem = (string)_itemList.Items[index*3];
+        _loadButton.Disabled = false;
+        _deleteButton.Disabled = false;
     }
 }
